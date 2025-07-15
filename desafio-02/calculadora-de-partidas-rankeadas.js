8f1c0c6e671c7fb290e25cab9687bd6a72d09453
Emilly Andrade <emilly.andrade.digital@gmail.com>
2025-07-15 14:12:36 -0300

    finalizei o exercício 02

--- desafio-02/calculadora-de-partidas-rankeadas.js
+++ desafio-02/calculadora-de-partidas-rankeadas.js
@@ -2,9 +2,24 @@ function calcularNivelRankeadas(totalDeVitorias, totalDeDerrotas){
     const resultadoFinal = totalDeVitorias - totalDeDerrotas;
     let nivelDoJogador;
 
-    if (totalDeVitorias < 10) {
+    if (resultadoFinal <= 10) {
         nivelDoJogador = "Ferro";
-    } else if (totalDeVitorias > 10 && totalDeVitorias <= 20) {
+    } else if (resultadoFinal >= 11 && resultadoFinal <= 20) {
         nivelDoJogador = "Bronze";
+    } else if (resultadoFinal >= 21 && resultadoFinal <= 50) {
+        nivelDoJogador = "Prata";
+    } else if (resultadoFinal >= 51 && resultadoFinal <= 80) {
+        nivelDoJogador = "Ouro";
+    } else if (resultadoFinal >= 81 && resultadoFinal <= 90) {
+        nivelDoJogador = "Diamante";
+    } else if (resultadoFinal >= 91 && resultadoFinal <= 100) {
+        nivelDoJogador = "Lendário";
+    } else {
+        nivelDoJogador = "Imortal";
     }
+
+    return `O Herói tem de saldo de ${resultadoFinal} está no nível ${nivelDoJogador}`;
 }
+
+console.log(calcularNivelRankeadas(100, 90));
+console.log(calcularNivelRankeadas(50, 20));
